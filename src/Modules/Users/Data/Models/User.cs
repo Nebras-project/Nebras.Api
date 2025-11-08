e94227a4be7a5ae4c5cc6661eae4a954dde56746
NULL <aqeelprogrammer@gmail.com>
2025-11-08 19:45:34 +0300

    feat: add User model.

---
+++ src/Modules/Users/Data/Models/User.cs
@@ -0,0 +1,19 @@
+﻿namespace Users.Data.Models;
+
+public class User
+{
+    public int Id { get; set; }
+    public string Email { get; set; } = default!;
+    public string Username { get; set; } = default!;
+    public string? PhoneNumber { get; set; }
+    public string? ProfileImage { get; set; }
+    public string HashedPassword { get; set; } = default!;
+    public int UserRoleId { get; set; }
+    public UserRole? UserRole { get; set; }
+
+
+    public static void HashPassword(string password)
+    {
+        // has password and set it to HashedPassword
+    }
+}
