563ac8f5a44f6a2e1ac9d3b9eb78f4831ad280cc
NULL <aqeelprogrammer@gmail.com>
2025-11-08 19:45:51 +0300

    feat: add StudentProfile.

---
+++ src/Modules/Users/Data/Models/StudentProfile.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Users.Data.Models
+{
+    public class StudentProfile
+    {
+        public int Id { get; set; }
+        // !temp
+        public string Grad { get; set; }
+        public int UserId { get; set; }
+        public User? User { get; set; }
+    }
+}
