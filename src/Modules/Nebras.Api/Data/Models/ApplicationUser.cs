b3c55a7f40c55b6c52b37fa4613646a8f82e14eb
NULL <aqeelprogrammer@gmail.com>
2025-11-13 23:19:16 +0300

    feat: add ApplicationUser.

---
+++ src/Modules/Nebras.Api/Data/Models/ApplicationUser.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Nebras.Api.Data.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public string Username { get; set; } = default!;
+        public string? ProfileImage { get; set; }
+    }
+}
