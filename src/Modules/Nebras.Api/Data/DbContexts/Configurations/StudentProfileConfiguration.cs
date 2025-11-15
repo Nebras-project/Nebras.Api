4aa78a5e720b78ceb4744a09e5c2c75b156e3c02
NULL <aqeelprogrammer@gmail.com>
2025-11-15 21:13:34 +0300

    feat: add StudentProfileConfiguration to db context.

---
+++ src/Modules/Nebras.Api/Data/DbContexts/Configurations/StudentProfileConfiguration.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Nebras.Api.Data.Models;
+
+namespace Nebras.Api.Data.DbContexts.Configurations
+{
+    public class StudentProfileConfiguration : IEntityTypeConfiguration<StudentProfile>
+    {
+        public void Configure(EntityTypeBuilder<StudentProfile> builder)
+        {
+            builder.ToTable("StudentProfiles");
+
+            
+            builder.HasKey(s => s.UserId);
+
+            
+            builder.Property(s => s.UserId)
+                   .ValueGeneratedNever();
+
+            
+            builder.HasOne(s => s.User)
+                   .WithOne(s => s.Student)
+                   .HasForeignKey<StudentProfile>(s => s.UserId)
+                   .IsRequired()
+                   .OnDelete(DeleteBehavior.Cascade);
+
+
+            builder.HasOne(s => s.grade)
+                   .WithMany(g => g.Students)
+                   .HasForeignKey(s => s.GradeId)
+                   .IsRequired()
+                   .OnDelete(DeleteBehavior.Restrict);
+
+        }
+    }
+}
--- src/Modules/Nebras.Api/Data/DbContexts/NebrasDbContext.cs
+++ src/Modules/Nebras.Api/Data/DbContexts/NebrasDbContext.cs
@@ -1,12 +1,18 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using Nebras.Api.Data.DbContexts.Configurations;
 using Nebras.Api.Data.Models;
 
 namespace Nebras.Api.Data.DbContexts
 {
     public class NebrasDbContext : IdentityDbContext<ApplicationUser, ApplicationIdentityRole, Guid>
     {
-        public DbSet<StudentProfile> Students { get; set; }
+        public DbSet<StudentProfile> StudentProfiles { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.ApplyConfiguration(new StudentProfileConfiguration());
+        }
     }
 }
