89763c9f84d54166ffa05e3a78f9f48826d1719a
NULL <aqeelprogrammer@gmail.com>
2025-11-15 22:28:58 +0300

    feat: add connection string and add the db context to the to IoC.

--- src/Modules/Nebras.Api/Program.cs
+++ src/Modules/Nebras.Api/Program.cs
@@ -1,4 +1,7 @@
 
+using Microsoft.EntityFrameworkCore;
+using Nebras.Api.Data.DbContexts;
+
 namespace Nebras.Api
 {
     public class Program
@@ -8,6 +11,10 @@ namespace Nebras.Api
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
+            var connectionString = builder.Configuration.GetConnectionString("NebrasConnectionString");
+            builder.Services.AddDbContext<NebrasDbContext>(
+                options => options.UseNpgsql(connectionString)
+            );
 
             builder.Services.AddControllers();
             // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
