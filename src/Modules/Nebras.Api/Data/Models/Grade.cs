94a985b77eb92f3bbd3612edf994005268f9c6e1
NULL <aqeelprogrammer@gmail.com>
2025-11-15 20:52:57 +0300

    feat: add Grade model.

---
+++ src/Modules/Nebras.Api/Data/Models/Grade.cs
@@ -0,0 +1,8 @@
+﻿namespace Nebras.Api.Data.Models
+{
+    public class Grade
+    {
+        public Guid Id { get; set; } = Guid.CreateVersion7();
+        public string Name { get; set; } = string.Empty;
+    }
+}
