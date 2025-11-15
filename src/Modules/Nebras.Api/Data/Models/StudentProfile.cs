f5a4fbabbe7eabc5df0fef2351a7986f2d57333f
NULL <aqeelprogrammer@gmail.com>
2025-11-15 23:59:28 +0300

    fix: remove auto creation of Guid (the id) in student profile.

--- src/Modules/Nebras.Api/Data/Models/StudentProfile.cs
+++ src/Modules/Nebras.Api/Data/Models/StudentProfile.cs
@@ -2,7 +2,7 @@
 {
     public class StudentProfile
     {
-        public Guid UserId { get; set; } = Guid.CreateVersion7();
+        public Guid UserId { get; set; }
         public ApplicationUser User { get; set; }
 
         public Guid GradeId { get; set; }
