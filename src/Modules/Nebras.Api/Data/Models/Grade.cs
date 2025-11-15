33c4421a86f7a563857900404b7797f6231f087e
NULL <aqeelprogrammer@gmail.com>
2025-11-15 21:11:23 +0300

    feat: add navigation property "Students" into Grade.

--- src/Modules/Nebras.Api/Data/Models/Grade.cs
+++ src/Modules/Nebras.Api/Data/Models/Grade.cs
@@ -4,5 +4,7 @@
     {
         public Guid Id { get; set; } = Guid.CreateVersion7();
         public string Name { get; set; } = string.Empty;
+
+        public ICollection<StudentProfile> Students { get; set; } = [];
     }
 }
